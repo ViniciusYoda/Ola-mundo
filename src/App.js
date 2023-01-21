dbbc78fc2f1ef1099adeab1adde0e90a5d94d551
Vinícius Yoda <vyoda4604@gmail.com>
2023-01-21 14:17:28 -0300

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+console.log(window.location);
 
+const pagina = window.location.pathname === '/' ? <div>Olá Mundo!</div> : <div>Sobre</div>
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return pagina
 }
 
 export default App;
